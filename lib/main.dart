b46a5c40ed0dc79111cef581e1391b6d7f3360db
Yusuf Emre Altun <yusufemrealtun08@gmail.com>
2023-02-04 20:13:43 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      title: "Burçlar App",
+      home: BurcListesi(),
+    );
   }
 }
