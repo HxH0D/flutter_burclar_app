74fb80724c731e1c47138705e8068fd31033e30b
Yusuf Emre Altun <yusufemrealtun08@gmail.com>
2023-02-05 17:48:39 +0300

    Veri kaynağı modeli modeli oluşturuldu ve veriler tek bir listeye çekildi

--- lib/burc_listesi.dart
+++ lib/burc_listesi.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_burclar_app/data/strings.dart';
+
+import 'model/burc.dart';
 
 class BurcListesi extends StatelessWidget {
-  const BurcListesi({super.key});
+  late List<Burc> tumBurclar;
+  BurcListesi() {
+    tumBurclar = veriKaynaginiHazirla();
+    debugPrint(tumBurclar.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +18,30 @@ class BurcListesi extends StatelessWidget {
       appBar: AppBar(
         title: Text("Burç Listesi"),
       ),
-      body: Center(child: Text("Burçlar buraya gelecek.")),
+      body: ListView(),
     );
+  }
+
+  List<Burc> veriKaynaginiHazirla() {
+    List<Burc> gecici = [];
+    for (int i = 0; i < 12; i++) {
+      var burcAdi = Strings.BURC_ADLARI[i];
+      var burcTarihi = Strings.BURC_TARIHLERI[i];
+      var burcDetayi = Strings.BURC_GENEL_OZELLIKLERI[i];
+      var burcKucukResim =
+          "${Strings.BURC_ADLARI[i].toLowerCase()}${i + 1}.png";
+      var burcBuyukResim =
+          "${Strings.BURC_ADLARI[i].toLowerCase()}_buyuk${i + 1}.png";
+
+      Burc eklenecekBurc = Burc(
+        burcAdi,
+        burcTarihi,
+        burcDetayi,
+        burcKucukResim,
+        burcBuyukResim,
+      );
+      gecici.add(eklenecekBurc);
+    }
+    return gecici;
   }
 }
