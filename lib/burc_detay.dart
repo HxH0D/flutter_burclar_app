4e6c606e0fe27c9487fee9d7ce2f20ddab0b6700
Yusuf Emre Altun <yusufemrealtun08@gmail.com>
2023-02-07 11:34:00 +0300

    pallette_generator paketi eklendi ve kayan apBar rengi resimlere göre değişebilir yapıldı

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -2,14 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_burclar_app/model/burc.dart';
+// palette generator paketi import
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   // secilen burc verilerini almak için değişken oluşturuldu
   final Burc secilenBurc;
 
   //BurcDetay widgeti kullanılırken secilen burc verilerinin gönderilmesi için requrired kullanıldı
 
-  const BurcDetay({super.key, required Burc this.secilenBurc});
+  const BurcDetay({super.key, required this.secilenBurc});
+
+  @override
+  State<BurcDetay> createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  //Rengii değiştirdiğimiz de kullanabilmek için rengi değişkene atıyoruz
+  Color appbatColor = Colors.transparent;
+  late PaletteGenerator _generator;
+
+  //widget ler deki buildler  1 den fazla defa çalışıyor olabilir bundan dolayı
+  //initState kullanıyoruz ki widget çalışınca initstate bir defa çalışsın ve sonra buil istediği kadar çalışsın
+  @override
+  void initState() {
+    super.initState();
+    appBarRenginibul();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +41,12 @@ class BurcDetay extends StatelessWidget {
             // ekranin ilk açılıştaki yükseklik
             expandedHeight: 250,
             pinned: true,
-            backgroundColor: Colors.pink,
+            backgroundColor: appbatColor,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(secilenBurc.burcAdi + " Burcu ve Özellikleri"),
+              title: Text(widget.secilenBurc.burcAdi + " Burcu ve Özellikleri"),
               centerTitle: true,
               background: Image.asset(
-                "images/" + secilenBurc.burcBuyukResim,
+                "images/" + widget.secilenBurc.burcBuyukResim,
                 fit: BoxFit.cover,
               ),
             ),
@@ -38,7 +57,7 @@ class BurcDetay extends StatelessWidget {
               margin: EdgeInsets.all(8),
               child: SingleChildScrollView(
                 child: Text(
-                  secilenBurc.burcDetayi,
+                  widget.secilenBurc.burcDetayi,
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
                 ),
               ),
@@ -47,5 +66,15 @@ class BurcDetay extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  // PaletGenarator islemi 1 2 saniye sürecek bir işlem olduğundan async ve await kullanıyoruz
+  void appBarRenginibul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+      AssetImage("images/" + widget.secilenBurc.burcBuyukResim),
+    );
+    appbatColor = _generator.dominantColor!.color;
+    //buildin tekrar çalışması ve değişkenlerin değişmesi için setState kullanıyoruz
+    setState(() {});
   }
 }
